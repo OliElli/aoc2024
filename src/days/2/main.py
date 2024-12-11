3d493106d83bb9d01fe051de639881db692f9e8e
Oliver Elliott <oliver.elliott@acronis.com>
2024-12-11 10:12:56 +0000

    day2 pt2

--- src/days/2/main.py
+++ src/days/2/main.py
@@ -1,54 +1,39 @@
 #!/usr/bin/env python3
 
-with open('example_input.txt') as f:
+from itertools import pairwise
+
+with open('input.txt') as f:
     data = f.read().splitlines()
 
 answer1 = 0
 answer2 = 0
 
-input = []
-for line in data:
-    input.append((line.split(' ')))
+input = [[int(i) for i in line.split()] for line in data]
 
-for line in input:
-    safe = False
-    prev_digit = int(line[0])
+def test(input):
     prev_diff = 0
-    for i in range(1,(len(line))):
-        difference = int(line[i]) - prev_digit
+    for previous, current in pairwise(input):
+        if not 0 < abs(previous - current) < 4:
+            return False
+        difference = current - previous
         if difference * prev_diff < 0:
-            safe = False
-            break
-        if 0 < abs(difference) < 4:
-            safe = True
-        else:
-            safe = False
-            break
-        prev_digit = int(line[i])
+            return False
+        if previous == current:
+            return False
         prev_diff = difference
-    if safe:
+    return True
+
+for report in input:
+    if test(report):
         answer1 += 1
-
-print(answer1)
-
-for line in input:
-    safe = 0
-    prev_digit = int(line[0])
-    prev_diff = 0
-    for i in range(1,(len(line))):
-        difference = int(line[i]) - prev_digit
-        if difference * prev_diff < 0:
-            safe = False
-            break
-        if 0 < abs(difference) < 4:
-            safe += 0
-        else:
-            safe += 1
-            break
-        prev_digit = int(line[i])
-        prev_diff = difference
-    if safe > 1:
-        answer2 += 1
+    else:
+        for i in range(len(report)):
+            report_copy = report.copy()
+            report_copy.pop(i)
+            if test(report_copy):
+                answer2 += 1
+                break
+answer2 += answer1
 
 print(answer1)
 print(answer2)
