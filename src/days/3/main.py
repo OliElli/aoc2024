7927f5fd056921f2331cf5694600cf752829e2b5
Oliver Elliott <oliver.elliott@acronis.com>
2024-12-11 10:42:39 +0000

    day3 pt2

--- src/days/3/main.py
+++ src/days/3/main.py
@@ -11,14 +11,27 @@ answer2 = 0
 input = "".join(data)
 
 # pt1
-ops = re.findall(r'mul\([0-9]+,[0-9]+\)', input)
-for op in ops:
-    a, b = map(int, re.findall(r'[0-9]+', op))
-    answer1 += a * b
+def calc(input):
+    answer = 0
+    ops = re.findall(r'mul\([0-9]+,[0-9]+\)', input)
+    for op in ops:
+        a, b = map(int, re.findall(r'[0-9]+', op))
+        answer += a * b
+    return answer
+
+answer1 = calc(input)
 
 #pt2
-donts = re.split(r'don\'t', input)
-print(donts)
+donts = re.split(r"don't\(\)", input)
+answer2 = calc(donts[0])
+
+for i, line in enumerate(donts):
+    if i == 0:
+        continue
+    dos = re.split(r"do\(\)", line)
+    for i in range(len(dos)):
+        if i > 0:
+            answer2 += calc(dos[i])
 
 print(answer1)
 print(answer2)
